17db851dc422257f0f51f6f2a9a9749c5fff9443
oriarh <111208485+oriarh@users.noreply.github.com>
2022-10-18 11:49:39 -0700

    pushing the final working code up

--- Develop/script.js
+++ Develop/script.js
@@ -3,21 +3,70 @@ $("#currentDay").text(todayDate);
 
 //retrieve the item stored in local storage
 function init() {
-    var event1 = JSON.parse(localStorage.getItem("event"));
+    var event1 = JSON.parse(localStorage.getItem("event1"));
+    var event2 = JSON.parse(localStorage.getItem("event2"));
+    var event3 = JSON.parse(localStorage.getItem("event3"));
+    var event4 = JSON.parse(localStorage.getItem("event4"));
+    var event5 = JSON.parse(localStorage.getItem("event5"));
+    var event6 = JSON.parse(localStorage.getItem("event6"));
+    var event7 = JSON.parse(localStorage.getItem("event7"));
+    var event8 = JSON.parse(localStorage.getItem("event8"));
+    var event9 = JSON.parse(localStorage.getItem("event9"));
+
     if(event1 !== null) {
         $('#9').val(event1);
+    };
+
+    if(event2 !== null) {
+        $('#10').val(event2);
     }
+
+    if(event3 !== null) {
+        $('#11').val(event3);
+    };
+
+    if(event4 !== null) {
+        $('#12').val(event4);
+    };
+
+    if(event5 !== null) {
+        $('#13').val(event5);
+    };
+
+    if(event6 !== null) {
+        $('#14').val(event6);
+    };
+
+    if(event7 !== null) {
+        $('#15').val(event7);
+    };
+
+    if(event8 !== null) {
+        $('#16').val(event8);
+    };
+
+    if(event9 !== null) {
+        $('#17').val(event9);
+    };
 };
 
-//store the input value in a variable
-$("#button1").on("click",function () {
-    console.log($('#9').val());
-    console.log("button is working");
-    localStorage.setItem("event",JSON.stringify($('#9').val()));
 
-    //Show the status message
-    $("#appointmentLogged").css("display","block");    
-});
+//Using each to store input variables for all - store the input value in a variable
+    $(".button1").on("click",function () {
+        
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+        localStorage.setItem("event"+$(this).attr("data-index"),JSON.stringify($(this).siblings(".textarea").val()));
+
+        //Show the status message
+        $("#appointmentLogged").css("display","block"); 
+    });
 
 init();
 
@@ -37,5 +86,3 @@ $("input").each(function(){
         $(this).addClass("future");
     };
 });
-
-console.log(+timenow);
